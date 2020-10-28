416980e5fcb206a2340ba562218bc8a9d2d496a9
Nicholas <36013469+nicklaranjeiro@users.noreply.github.com>
2020-10-28 17:28:05 -0400

    added genres to home page and things for git pages

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -12,6 +12,7 @@ export class MovieListComponent implements OnInit {
 
   posterPath: string = 'https://image.tmdb.org/t/p/w154';
   genresArray: any[] = [];
+  genresDictionary;
   @Input() movieInfo;
 
   ngOnInit(): void {
@@ -19,6 +20,28 @@ export class MovieListComponent implements OnInit {
       console.log('result', result);
       this.genresArray = result.genres;
     });
+
+    this.genresDictionary = {
+      '28': 'Action',
+      '12': 'Adventure',
+      '16': 'Animation',
+      '35': 'Comedy',
+      '80': 'Crime',
+      '99': 'Documentary',
+      '18': 'Drama',
+      '10751': 'Family',
+      '14': 'Fantasy',
+      '36': 'History',
+      '27': 'Horror',
+      '10402': 'Music',
+      '9648': 'Mystery',
+      '10749': 'Romance',
+      '878': 'Science Fiction',
+      '10770': 'TV Movie',
+      '53': 'Thriller',
+      '10752': 'War',
+      '37': 'Western',
+    };
 
     //getYear, pass in release date to getFullYear return the year
   }
